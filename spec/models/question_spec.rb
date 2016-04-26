ca91d5addfca4d1d97d81985366d20a92072fa7d
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-04-26 13:13:59 +0500

    Тест модели Question изменен на формат shoulda-matchers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Question, type: :model do
-  it 'validates presence of title' do
-    expect(Question.new(body: 'sdfsdfsdf')).to_not be_valid
-  end
-  it 'validates presence of body' do
-    expect(Question.new(title: 'sdfsdfsdf')).to_not be_valid
-  end
+describe Question do
+  it {should validate_presence_of :title }
+  it {should validate_presence_of :body }
 end

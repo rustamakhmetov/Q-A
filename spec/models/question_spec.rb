9a2acbb0eae30fc8c41735bc884804915ed0a920
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-05-18 14:01:47 +0500

    Question accepts nested attributes for attachments

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,4 +7,5 @@ describe Question do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should validate_presence_of :user_id}
+  it { should accept_nested_attributes_for :attachments}
 end

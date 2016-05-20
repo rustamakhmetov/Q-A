26681b86a2c498193732b2acfc766fbe2c0ac7a6
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-05-20 14:46:54 +0500

    Specs for answer with nested attributes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it {should belong_to :user}
   it {should belong_to :question}
-  it {should have_many :attachments }
+  it {should have_many(:attachments).dependent(:destroy) }
   it {should validate_presence_of :body}
   it {should validate_presence_of :user_id}
   it {should validate_presence_of :question_id}
@@ -31,6 +31,48 @@ RSpec.describe Answer, type: :model do
         it { should_not change { answer1.reload.accept }.from(false) }
         it { should_not change { answer2.reload.accept }.from(true) }
       end
+    end
+  end
+
+  describe "Attachments" do # the has_many association
+    let(:user) { create(:user) }
+    let(:question) { create(:question) }
+
+    subject(:test_file) do
+      lambda do
+        ActionDispatch::Http::UploadedFile.new({
+                                                   :filename => 'spec_helper.rb',
+                                                   :tempfile => File.new("#{Rails.root}/spec/spec_helper.rb")
+                                               })
+      end
+    end
+
+    describe "save with" do
+      it "valid attributes" do
+        q = Answer.new(attributes_for(:answer).merge(user: user, question: question, attachments_attributes: [{file: test_file.call}]))
+        expect{ q.save }.to change(Answer, :count).by(1).and change(Attachment, :count).by(1)
+      end
+
+      it 'invalid attributes' do
+        q = Answer.new(attributes_for(:answer).merge(user: user, question: question, attachments_attributes: [{file: nil}]))
+        expect{ q.save }.to change(Answer, :count).by(1).and change(Attachment, :count).by(0)
+      end
+    end
+
+    it "allows destroy attribute" do
+      attachments_attributes = 3.times.collect { {file: test_file.call } }
+
+      expect(Attachment.all.count).to eq 0
+      q = Answer.new(attributes_for(:answer).merge(user: user, question: question, attachments_attributes: attachments_attributes))
+      q.save
+      expect(Answer.all.count).to eq 1
+      expect(Attachment.all.count).to eq 3
+
+      attachments_attributes = q.attachments.each.collect { |a| {id: a.id, _destroy: "1"} }
+      expect{
+        q.attachments_attributes = attachments_attributes
+        q.save
+      }.to change(Answer, :count).by(0).and change(Attachment, :count).by(-3)
     end
   end
 end

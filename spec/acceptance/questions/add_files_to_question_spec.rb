0c42774a559a8b96efbb03256d8b48cb8f2ead34
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-05-23 13:31:28 +0500

    User adds multiple files when asks question

--- spec/acceptance/questions/add_files_to_question_spec.rb
+++ spec/acceptance/questions/add_files_to_question_spec.rb
@@ -20,6 +20,21 @@ feature 'Add files to question', %q{
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: "#{Rails.root}/spec/tmp/uploads/attachment/file/1/spec_helper.rb"
-
   end
+
+  scenario 'User adds multiple files when asks question', js: true do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+    within(:xpath, '//div[@id="attachments"]/div[@class="nested-fields"][2]') do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: "#{Rails.root}/spec/tmp/uploads/attachment/file/2/spec_helper.rb"
+    expect(page).to have_link 'spec_helper.rb', href: "#{Rails.root}/spec/tmp/uploads/attachment/file/3/spec_helper.rb"
+  end
+
+
 end
